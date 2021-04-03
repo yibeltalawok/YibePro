6ba4f450adf4ba4ddfc57ab5b7c8f8e8a0039f19
Tesfamichael1074 <hope.miky1074@gmail.com>
2021-04-03 23:10:09 +0300

    some updates for production registration

--- common/models/production-history.js
+++ common/models/production-history.js
@@ -2,4 +2,28 @@
 
 module.exports = function (Productionhistory) {
 
+
+    Productionhistory.deleteAll = (cb) => {
+        try {
+            Productionhistory.find({}, (err, res) => {
+            cb(null, res)
+          })
+        } catch (error) {
+          throw new Error("Internal server error try again");
+        }
+      }
+      Productionhistory.remoteMethod("deleteAll", {
+        description: "Delete all Productionhistory",
+        // accepts: [],
+        returns: {
+          type: "object",
+          root: true
+        },
+        http: {
+          verb: "delete",
+          path: "/deleteAll"
+        }
+      });
+
+
 };
