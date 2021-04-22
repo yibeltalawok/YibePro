c2eeb093690a1c20d3eed20d7b4efe4e87a044b2
Ubuntu <ubuntu@ip-172-31-36-243.us-east-2.compute.internal>
2021-04-22 08:31:55 +0000

    added SSL support for secure connection with custom private key

--- server/server.js
+++ server/server.js
@@ -1,33 +1,31 @@
-/* eslint no-console:0 */
+'use strict';
 
-const loopback = require("loopback");
-const boot = require("loopback-boot");
-// load env vars
-const result = require("dotenv").config();
+	const loopback = require('loopback'); const boot = require('loopback-boot');
 
-if (result.error) {
-  throw result.error;
-}
+	const http = require('http'); const https = require('https'); const sslConfig = require('./ssl-config');
 
-const app = loopback();
-require("loopback-row-count-mixin")(app);
-require("loopback-disable-method-mixin")(app);
+	const app = module.exports = loopback();
 
-app.start = function() {
-  // start the web server
-  return app.listen(() => {
-    app.emit("started");
-    console.log("Server started ");
-    const baseUrl = app.get("url").replace(/\/$/, "");
-    if (app.get("loopback-component-explorer")) {
-      const explorerPath = app.get("loopback-component-explorer").mountPath;
-      console.log("Browse your REST API at %s%s", baseUrl, explorerPath);
-    }
-  });
-};
-boot(app, __dirname, err => {
-  if (err) throw err;
-  if (require.main === module) app.start();
-});
+	// boot scripts mount components like REST API
+	boot(app, __dirname);
 
-module.exports = app;
+	app.start = function(httpOnly) { if (httpOnly === undefined) { httpOnly = process.env.HTTP;
+	  }
+	  let server = null; if (!httpOnly) { const options = { key: sslConfig.privateKey, cert: sslConfig.certificate,
+	    };
+	    server = https.createServer(options, app);
+	  } else {
+	    server = http.createServer(app);
+	  }
+	  server.listen(app.get('port'), function() { const baseUrl = (httpOnly ? 'http://' : 'https://') + app.get('host') + ':' + app.get('port'); 
+	    app.emit('started', baseUrl); console.log('LoopBack server listening @ %s%s', baseUrl, '/'); if (app.get('loopback-component-explorer')) {
+	      const explorerPath = app.get('loopback-component-explorer').mountPath; console.log('Browse your REST API at %s%s', baseUrl, 
+	      explorerPath);
+	    }
+	  });
+	  return server;
+	};
+
+	// start the server if `$ node server.js`
+	if (require.main === module) { app.start();
+	}
