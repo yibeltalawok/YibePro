8784b48103dd7d5763b19e4d8a8ed003ff8341bf
Tesfamichael <hope.miky1074@gmail.com>
2021-04-24 00:12:41 -0700

    added delete all endpoint in notification model

--- common/models/notification.js
+++ common/models/notification.js
@@ -19,21 +19,37 @@ module.exports = function(Notification) {
       });
       
 
-
-
-
-
-
-
-
-
-
-
-
       Notification.observe('after delete', function(ctx, next) {
         if(ctx.isNewInstance) console.log("New Instance Added")
         next()
       });
+
+
+      Notification.deleteAll = (cb) => {
+        try {
+            Notification.find({}, (err, res) => {
+                cb(null, res)
+            })
+        } catch (error) {
+            throw new Error("Internal server error try again");
+        }
+    }
+    
+    Notification.remoteMethod("deleteAll", {
+        description: "QR code generator",
+        // accepts: [],
+
+        returns: {
+            type: "object",
+            root: true
+        },
+
+        http: {
+            verb: "delete",
+            path: "/deleteAll"
+
+        }
+    })
 
 
 };
