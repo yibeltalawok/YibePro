62a32df6f5c31fac5a4b7a8894fee14f2b253d11
TemuSoft <temusoft2012@gmail.com>
2021-04-27 08:14:26 -0300

    Before create new branch

--- common/models/bundle-history.js
+++ common/models/bundle-history.js
@@ -198,4 +198,93 @@ module.exports = function(BundleHistory) {
     }
 
     
+    BundleHistory.remoteMethod("getTotalad", {
+      description: "Get date specific Total Amount Done per line",
+  
+      accepts: [{
+        arg: "date",
+        type: "string",
+        required: true
+      },
+      ],
+  
+      returns: {
+        type: "object",
+        root: true
+      },
+  
+      http: {
+        verb: "post",
+        path: "/getTotalad"
+      }
+    });
+  
+    BundleHistory.getTotalad = (
+      date,
+      cb
+    ) => {
+      var data = [];
+      BundleHistory.find({where: { date: {like: date}, newStatus: 'so' },}).then( res => {
+        var distlineNumber = [];
+        var existlineNumber = 0;
+
+      for (let i = 0; i < res.length; i++) {
+        for (let l = 0; l < distlineNumber.length; l++) {
+          if(res[i].lineNumber === distlineNumber[l]){
+            existlineNumber =1;
+            break;
+          }else existlineNumber = 0;
+        }
+        if(existlineNumber === 0)
+          distlineNumber.push(res[i].lineNumber);
+      }
+
+      for (let l = 0; l < distlineNumber.length; l++) {
+        var counter = 0;
+        for (let i = 0; i < res.length; i++) {
+          if(distlineNumber[l] === res[i].lineNumber)
+            counter += 1;
+        }
+
+        data.push({line: distlineNumber[l], totalad: counter});
+      }
+
+        cb(null, data)
+      })
+
+      
+    }
+  
+    BundleHistory.getTodayBundleHistoryAll = (date, cb)=>{
+      var data = {ci: 0, co: 0, si: 0, so: 0, fi: 0, fo: 0, pi: 0, po: 0, };
+
+      var filter = {include: ['ScannedOrderStatus'], where: {date: {like: date}}};
+      BundleHistory.find(filter).then(res=>{
+        for(var i=0; i<res.length;i++){
+          if(res[i].__data.newStatus === "ci") data.ci += (parseFloat(res[i].__data.ScannedOrderStatus.__data.to)-parseFloat(res[i].__data.ScannedOrderStatus.__data.from) +1)
+          else if(res[i].__data.newStatus === "co") data.co += (parseFloat(res[i].__data.ScannedOrderStatus.__data.to)-parseFloat(res[i].__data.ScannedOrderStatus.__data.from) +1)
+          else if(res[i].__data.newStatus === "si") data.si += (parseFloat(res[i].__data.ScannedOrderStatus.__data.to)-parseFloat(res[i].__data.ScannedOrderStatus.__data.from) +1)
+          else if(res[i].__data.newStatus === "so") data.so += (parseFloat(res[i].__data.ScannedOrderStatus.__data.to)-parseFloat(res[i].__data.ScannedOrderStatus.__data.from) +1)
+          else if(res[i].__data.newStatus === "fi") data.fi += (parseFloat(res[i].__data.ScannedOrderStatus.__data.to)-parseFloat(res[i].__data.ScannedOrderStatus.__data.from) +1)
+          else if(res[i].__data.newStatus === "fo") data.fo += (parseFloat(res[i].__data.ScannedOrderStatus.__data.to)-parseFloat(res[i].__data.ScannedOrderStatus.__data.from) +1)
+          else if(res[i].__data.newStatus === "pi") data.pi += (parseFloat(res[i].__data.ScannedOrderStatus.__data.to)-parseFloat(res[i].__data.ScannedOrderStatus.__data.from) +1)
+          else if(res[i].__data.newStatus === "po") data.po += (parseFloat(res[i].__data.ScannedOrderStatus.__data.to)-parseFloat(res[i].__data.ScannedOrderStatus.__data.from) +1)
+        }
+        cb(null, data)
+      });
+    }
+
+    BundleHistory.remoteMethod("getTodayBundleHistoryAll", {
+      description: "Get date specific bundle history",
+  
+      accepts: {arg: "date", type: "string", required: true},
+  
+      returns: {type: "object", root: true },
+  
+      http: {
+        verb: "get",
+        path: "/getTodayBundleHistoryAll"
+      }
+    });
+
 };
